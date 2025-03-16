199cd3ebfb73c6911928387bab25bcd60d591969
Eranda Madusanka <madushankaeranda56@gmail.com>
2025-03-16 20:34:11 +0530

    update 3

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $destinations = [
+        ['name' => 'Sigiriya Rock', 'desc' => 'Climb the ancient rock fortress with stunning views.'],
+        ['name' => 'Galle Fort', 'desc' => 'Stroll through history in this colonial seaside fort.'],
+        ['name' => 'Yala National Park', 'desc' => 'Spot leopards and elephants on a thrilling safari.']
+    ];
+    return view('travel', ['destinations' => $destinations]);
 });
